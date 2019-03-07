c7902298c34428b7585f5dda8648a7844a1b6e6e
balogbalazs <b.balazs79@gmail.com>
2019-03-07 14:44:36 +0100

    HomeController Sintax Error javítás

--- Kutatas_core/Kutatas_core/Controllers/HomeController.cs
+++ Kutatas_core/Kutatas_core/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace Kutatas_core.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Login()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
